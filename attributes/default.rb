0b56543011ac835e67e2e0ca5089113a7129a910
Phil Bolduc <philbolduc@sierrasystems.com>
2015-03-28 23:01:16 -0700

    Set version to 3.0.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,9 @@
-default['eventstore']['source_uri'] = "https://github.com/EventStore/EventStore/releases/download/OSS-v3.0.0-rc9/EventStore-OSS-Linux-v3.0.0-rc9.tar.gz"
+
+default['eventstore']['source_uri'] = "https://github.com/EventStore/EventStore/releases/download/oss-v3.0.3/EventStore-OSS-Linux-v3.0.3.tar.gz"
 
 default['eventstore']['install_dir'] = '/usr/local/eventstore/'
 # 2.5.0rc4 had the binaries in a subfolder, this would allow consumer of cookbook to override executable dir if using non-standard archive
-default['eventstore']['executable_dir'] = "#{node['eventstore']['install_dir']}EventStore-OSS-Linux-v3.0.0-rc9/" 
+default['eventstore']['executable_dir'] = "#{node['eventstore']['install_dir']}EventStore-OSS-Linux-v3.0.3/" 
 default['eventstore']['user'] = 'eventstore'
 default['eventstore']['config_dir'] = '/etc/eventstore/'
 default['eventstore']['config_file'] = "#{node['eventstore']['config_dir']}config.yaml"
